f3193966ed802daeec4365ba5504f62dbbd1676c
NatherBaneSalameh <natherbanisalameh@gmail.com>
2024-12-29 10:55:30 -0800

    frontend register

--- Project 4 Starter Code/frontend/src/App.js
+++ Project 4 Starter Code/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./App.css";
+import Register from "./components/shared components/Register/Register"
 const App = () => {
   return (
    <div className="App">
-      <h1>Hello World!</h1>
+    <Register />
     </div>
   )
 }
---
+++ Project 4 Starter Code/frontend/src/components/shared components/Register/Register.js
@@ -0,0 +1,58 @@
+import React from "react";
+import axios from "axios";
+import "./style.css";
+import { useState } from "react";
+const Register = () => {
+  const [userInfo, setUserInfo] = useState("");
+  const [isRegister, setIsRegister] = useState(false);
+  const [response, setResponse] = useState("");
+
+  const createRegister = () => {
+    axios
+      .post(`http://localhost:5000/users/register`, userInfo)
+      .then((res) => {
+        setResponse(res);
+        setIsRegister(true);
+        console.log( "pass",response);
+
+      })
+      .catch((err) => {
+        setResponse(err);
+        setIsRegister(false);
+        console.log( "err",response);
+        
+      });
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input
+        type="text"
+        placeholder="UserName"
+        onChange={(e) => {
+          setUserInfo({ ...userInfo, userName: e.target.value });
+        }}
+      />
+      <input
+        type="email"
+        placeholder="Email..."
+        onChange={(e) => {
+          setUserInfo({ ...userInfo, email: e.target.value });
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password..."
+        onChange={(e) => {
+          setUserInfo({ ...userInfo, password: e.target.value });
+        }}
+      />
+      <button onClick={createRegister}>Register</button>
+      
+      {isRegister?<p>{response.data.message}</p>:<p>{response.response.data.message}</p>}
+      
+    </div>
+  );
+};
+export default Register 
