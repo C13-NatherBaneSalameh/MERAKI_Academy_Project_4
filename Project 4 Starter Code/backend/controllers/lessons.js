3f968ba4d094b95703daec372190874108e5f21e
NatherBaneSalameh <natherbanisalameh@gmail.com>
2024-12-29 06:03:45 -0800

    delete & update lesson

--- Project 4 Starter Code/backend/controllers/lessons.js
+++ Project 4 Starter Code/backend/controllers/lessons.js
@@ -2,51 +2,92 @@ const lessonsModel = require("../models/lessons");
 
 const createNewLesson = (req, res) => {
   const { title, description, courseId, video } = req.body;
-  const teacherId =req.token.userId
+  const teacherId = req.token.userId;
   const newLesson = new lessonsModel({
     title,
-description,
-courseId,
-video,
-teacherId
+    description,
+    courseId,
+    video,
+    teacherId,
   });
 
   newLesson
-  .save()
-  .then((lessone) => {
-    res.status(201).json({
-      success: true,
-      message: `lessone created`,
-      lessone: lessone,
+    .save()
+    .then((lessone) => {
+      res.status(201).json({
+        success: true,
+        message: `lessone created`,
+        lessone: lessone,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
     });
-  })
-  .catch((err) => {
-    res.status(500).json({
-      success: false,
-      message: `Server Error`,
-      err: err.message,
-    });
-  });
 };
-const getAllLessons=(req,res)=>{
-  lessonsModel.find()
+const getAllLessons = (req, res) => {
+  lessonsModel
+    .find()
+    .then((result) => {
+      if (result.length === 0) {
+        res.status(400).json("No lessons yet ");
+      } else {
+        res.status(200).json({
+          success: true,
+          message: " All the lessons",
+          lessone: result,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+
+        message: "Server Error",
+
+        err: err.message,
+      });
+    });
+};
+const deleteLessonsById = (req, res) => {
+  const id = req.params.id;
+
+  lessonsModel
+    .deleteOne({ _id: id })
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "Lesson deleted ",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+        err: err.message,
+      });
+    });
+};
+const updateLessonById = (req, res) => {
+  const id = req.params.id;
+  const { title, description, video } = req.body;
+  lessonsModel
+  .findOneAndUpdate({_id: id},{title:title , description:description ,video:video}, {new:true})
   .then((result)=>{
-    
     res.status(200).json({
       success: true,
-      message:" All the lessons",
-      lessone: result,
-
+      message: "Lesson updated",
+      lesson: result
     })
-  }).catch((err)=>{
+  })
+  .catch((err)=>{
   res.status(500).json({
     success: false,
-
-message: "Server Error",
-
-err:err.message
+    message: "Server Error",
+    err: err.message
   })
-  })
-
-}
-module.exports={createNewLesson ,getAllLessons }
+  })};
+module.exports = { createNewLesson, getAllLessons, deleteLessonsById ,updateLessonById};
--- Project 4 Starter Code/backend/routes/lessons.js
+++ Project 4 Starter Code/backend/routes/lessons.js
@@ -1,9 +1,11 @@
 const express=require("express")
-const {createNewLesson ,getAllLessons}=require("../controllers/lessons")
+const {createNewLesson ,getAllLessons,deleteLessonsById ,updateLessonById}=require("../controllers/lessons")
 const authentication=require("../middleware/authentication")
 const authorization = require("../middleware/authorization")
 
 const lessoneRouter =express.Router()
 lessoneRouter.post("/",authentication ,authorization("CREATE_LESSON"),createNewLesson)
 lessoneRouter.get("/allLessons",getAllLessons)
+lessoneRouter.delete("/:id",authentication,deleteLessonsById)
+lessoneRouter.put("/:id",updateLessonById)
 module.exports=lessoneRouter
