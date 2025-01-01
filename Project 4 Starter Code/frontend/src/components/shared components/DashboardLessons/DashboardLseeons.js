5d3f647c680d1afebff10a18f5b1e6a1e065a038
NatherBaneSalameh <natherbanisalameh@gmail.com>
2025-01-01 06:27:44 -0800

    comment lessone

--- Project 4 Starter Code/frontend/src/components/shared components/DashboardLessons/DashboardLseeons.js
+++ Project 4 Starter Code/frontend/src/components/shared components/DashboardLessons/DashboardLseeons.js
@@ -5,15 +5,16 @@ import { useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
 import { UserContext } from "../../../App";
 const DashboardLseeons = () => {
-  const [isClickedToUpdate, setIsClickedToUpdate] = useState(false)
-  const [newInfoLessons, setNewInfoLessons] = useState({})
+  const [isClickedToUpdate, setIsClickedToUpdate] = useState(false);
+  const [newInfoLessons, setNewInfoLessons] = useState({});
+  const [comments, setComments] = useState({});
   const { token, lesson, setLesson } = useContext(UserContext);
 
   const headers = {
     Authorization: `Bearer ${token}`,
   };
   const navigate = useNavigate();
-  const {id} = useParams();
+  const { id } = useParams();
   console.log(id);
   const addLessons = () => {
     navigate(`/addLesson/${id}`);
@@ -22,91 +23,134 @@ const DashboardLseeons = () => {
     axios
       .get(`http://localhost:5000/lessons/${id}`, { headers })
       .then((res) => {
-        setLesson(res.data.lessone)
+        setLesson(res.data.lessone);
         console.log(res.data.lessone);
-        
       })
-      .catch((err)=>{
-console.log(err);
-      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   useEffect(() => {
-    getLessonsById()
-    
-    
-  }, [])
-  const updatedLesson=(id)=>{
+    getLessonsById();
+  }, []);
+  const updatedLesson = (id) => {
     axios
-    .put(`http://localhost:5000/lessons/${id}`, newInfoLessons, { headers })
-    .then((res)=>{
-      const lessoneAfterUpdate=lesson.map((ele,ind)=>{
-        if(ele._id===res.data.lesson._id){
-          ele.title=res.data.lesson.title
-          ele.description=res.data.lesson.description
-          ele.video=res.data.lesson.video
-        }
-        return ele;
+      .put(`http://localhost:5000/lessons/${id}`, newInfoLessons, { headers })
+      .then((res) => {
+        const lessoneAfterUpdate = lesson.map((ele, ind) => {
+          if (ele._id === res.data.lesson._id) {
+            ele.title = res.data.lesson.title;
+            ele.description = res.data.lesson.description;
+            ele.video = res.data.lesson.video;
+          }
+          return ele;
+        });
+        setLesson(lessoneAfterUpdate);
+        setIsClickedToUpdate(false);
       })
-      setLesson(lessoneAfterUpdate)
-      setIsClickedToUpdate(false)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const addComment = (id) => {
+    axios
+    .post(`http://localhost:5000/comments/${id}`, comments, { headers })
+    .then((res)=>{
+      getLessonsById()
 
     })
     .catch((err)=>{
       console.log(err);
+      
 
     })
-
-  }
-
   
+  };
 
   return (
     <div>
-        <div>
-      <button onClick={addLessons}>addLesson</button>
+      <div>
+        <button onClick={addLessons}>addLesson</button>
       </div>
       <div>
-        {lesson?.map((ele,ind)=>{
-            return <div>
-                <iframe src={ele.video} type="video/mp4"/>
-                <p>{ele.title}</p>
-                <p>{ele.description}</p>
-                
-                {isClickedToUpdate?
-                <div>
-                  <input type="text" placeholder="title" onChange={(e) => {
-                  setNewInfoLessons({
-                    ...newInfoLessons,
-                    title: e.target.value,
-                  });
-                }}/>
-                  <textarea typeof="text" placeholder="description" onChange={(e) => {
-                  setNewInfoLessons({
-                    ...newInfoLessons,
-                    description: e.target.value,
-                  });
-                }}></textarea>
-                  <input placeholder="video" onChange={(e) => {
-                  setNewInfoLessons({
-                    ...newInfoLessons,
-                    video: e.target.value
-                  });
-                }} />
-                  <button id={ele._id} onClick={(e)=>{
-                    updatedLesson(e.target.id)
-                  }}> Update</button>
-                  
-                  
-                  
-                  
-                  
-                  
-                  </div> :<button onClick={()=>{
-                  setIsClickedToUpdate(true)
-                }} > update </button>}
-            </div>
-        })}
+        {lesson?.map((ele, ind) => {
+          return (
+            <div>
+              <iframe src={ele.video} type="video/mp4" />
+              <p>{ele.title}</p>
+              <p>{ele.description}</p>
+              {ele.comments.map(o=><p>comment : {o.comment}</p>)}
 
+
+              <textarea
+                placeholder="Comment"
+                onChange={(e) => {
+                  setComments({ ...comments, comment: e.target.value });
+                }}
+              ></textarea>
+              <button
+                id={ele._id}
+                onClick={(e) => {
+                  addComment(e.target.id);
+                }}
+              >
+                comment
+              </button>
+
+              {isClickedToUpdate ? (
+                <div>
+                  <input
+                    type="text"
+                    placeholder="title"
+                    onChange={(e) => {
+                      setNewInfoLessons({
+                        ...newInfoLessons,
+                        title: e.target.value,
+                      });
+                    }}
+                  />
+                  <textarea
+                    typeof="text"
+                    placeholder="description"
+                    onChange={(e) => {
+                      setNewInfoLessons({
+                        ...newInfoLessons,
+                        description: e.target.value,
+                      });
+                    }}
+                  ></textarea>
+                  <input
+                    placeholder="video"
+                    onChange={(e) => {
+                      setNewInfoLessons({
+                        ...newInfoLessons,
+                        video: e.target.value,
+                      });
+                    }}
+                  />
+                  <button
+                    id={ele._id}
+                    onClick={(e) => {
+                      updatedLesson(e.target.id);
+                    }}
+                  >
+                    {" "}
+                    Update
+                  </button>
+                </div>
+              ) : (
+                <button
+                  onClick={() => {
+                    setIsClickedToUpdate(true);
+                  }}
+                >
+                  {" "}
+                  update{" "}
+                </button>
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
